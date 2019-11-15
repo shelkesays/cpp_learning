cf8e3c6b69ebc7f15ee61f473acc642b54485175
Rahul Shelke <srahul07@gmail.com>
2019-11-15 12:52:11 +0530

    Adding first few cpp programs

---
+++ first/1.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  // ask for the person's name
+  std::cout << "Please enter your first name: ";
+
+  // read the name
+  std::string name;      // define name
+  std::cin >> name;
+
+  // write a greeting
+  std::cout << "Hello, " << name << "!" << std::endl;
+  return 0;
+}
