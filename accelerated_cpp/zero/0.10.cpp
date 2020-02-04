0eedd07c1c9920bb73f19965943e1316b824fd72
Rahul Shelke <srahul07@gmail.com>
2020-02-04 16:55:03 +0530

    Moving accelerated cpp examples to its own folder

---
+++ accelerated_cpp/zero/0.10.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Hello,\nworld!" << std::endl;
+  return 0;
+}
---
+++ accelerated_cpp/zero/0.3.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+  std::cout << "This is a \t tab example." << std::endl;
+  return 0;
+}
