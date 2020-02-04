5fa0164bd7a07eed614a8c21264ba8f285fdb511
Rahul Shelke <srahul07@gmail.com>
2020-02-04 17:10:25 +0530

    removing linker error

--- beginning_cpp/CompilerWarnings/main.cpp
+++ beginning_cpp/CompilerWarnings/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-extern int x;
+//extern int x;
 
 int main()
 {
 //	int favorite_number;
 //	favorite_number = 100;
 //	std::cout << favorite_number << std::endl;
-//	std::cout << "Hello World." << std::endl;
-	std::cout << x;
+	std::cout << "Hello World." << std::endl;
+//	std::cout << x;
 	return 0;
 }
